b2f7c77459534a9a6e68866ac34ddd50d9ea7306
bqyx <llw@ysigiatec.com>
2013-04-24 17:55:34 +0800

    Signed-off-by: bqyx <llw@ysigiatec.com>

--- com.sg.vim.sync/src/com/sg/vim/sync/Sync.java
+++ com.sg.vim.sync/src/com/sg/vim/sync/Sync.java
@@ -1,5 +1,8 @@
 package com.sg.vim.sync;
 
+import java.sql.Connection;
+import java.util.Iterator;
+
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
@@ -9,83 +12,114 @@ import org.eclipse.core.runtime.jobs.JobChangeAdapter;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
+import com.sg.sqldb.DDB;
+import com.sg.sqldb.utility.SQLResult;
+import com.sg.sqldb.utility.SQLRow;
+import com.sg.sqldb.utility.SQLUtil;
+
 /**
  * The activator class controls the plug-in life cycle
  */
-public class Sync extends AbstractUIPlugin  {
+public class Sync extends AbstractUIPlugin {
 
-  // The plug-in ID
-  public static final String PLUGIN_ID = "com.sg.vim.sync"; //$NON-NLS-1$
+	// The plug-in ID
+	public static final String PLUGIN_ID = "com.sg.vim.sync"; //$NON-NLS-1$
 
-  private static final int DELAYHOURS = 8;
+	private static final int DELAYHOURS = 8;
 
-  // The shared instance
-  private static Sync plugin;
+	// The shared instance
+	private static Sync plugin;
 
-  private Job job;
+	private Job job;
 
-  private JobChangeAdapter listener;
+	private JobChangeAdapter listener;
 
-  /**
-   * The constructor
-   */
-  public Sync() {
-  }
+	/**
+	 * The constructor
+	 */
+	public Sync() {
+	}
 
-  /*
-   * (non-Javadoc)
-   * 
-   * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
-   */
-  public void start(BundleContext context) throws Exception {
-    super.start(context);
-    plugin = this;
-    startSyncProductCodeData();
-  }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext
+	 * )
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+		testSQLInteface();
+//		startSyncProductCodeData();
+	}
 
-  private void startSyncProductCodeData() {
-    job = new Job("SyncProductCodeData"){
+	private void testSQLInteface() {
+		//获得SQL Server连接
+		String query = "select materialcode,materialname,vehicletype "
+				+ "from VI_CBO_ITEMMASTER where factorycode='001' and materialcode like '88%'";
 
-      @Override
-      protected IStatus run(IProgressMonitor monitor) {
-        // TODO Auto-generated method stub
-        return Status.OK_STATUS;
-      }
-      
-    };
-    listener = new JobChangeAdapter(){
-      @Override
-      public void done(IJobChangeEvent event) {
-        job.schedule(getDelay());
-      }
-    };
-    job.addJobChangeListener(listener);
-  }
+		//测试查询语句
+		try {
+			SQLResult result = SQLUtil.SQL_QUERY("erp",query);
+			Iterator<SQLRow> iter = result.iterator();
+			while(iter.hasNext()){
+				SQLRow row = iter.next();
+				Object materialcode = row.getValue("materialcode");//成品码
+				Object materialname = row.getValue("materialname");//车型名称
+				Object vehicletype = row.getValue("vehicletype");//公告车型
+				System.out.println(materialcode+">>"+materialname+">>"+vehicletype);
+			}
+			System.out.println(result.size());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-  /*
-   * (non-Javadoc)
-   * 
-   * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
-   */
-  public void stop(BundleContext context) throws Exception {
-    plugin = null;
-    job.removeJobChangeListener(listener);
-    job.cancel();
-    super.stop(context);
-  }
+	private void startSyncProductCodeData() {
+		job = new Job("SyncProductCodeData") {
 
-  /**
-   * Returns the shared instance
-   * 
-   * @return the shared instance
-   */
-  public static Sync getDefault() {
-    return plugin;
-  }
+			@Override
+			protected IStatus run(IProgressMonitor monitor) {
+				// TODO Auto-generated method stub
+				return Status.OK_STATUS;
+			}
 
+		};
+		listener = new JobChangeAdapter() {
+			@Override
+			public void done(IJobChangeEvent event) {
+				job.schedule(getDelay());
+			}
+		};
+		job.addJobChangeListener(listener);
+	}
 
-  private long getDelay() {
-    return DELAYHOURS*60*60*1000;
-  }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext
+	 * )
+	 */
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		job.removeJobChangeListener(listener);
+		job.cancel();
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 * 
+	 * @return the shared instance
+	 */
+	public static Sync getDefault() {
+		return plugin;
+	}
+
+	private long getDelay() {
+		return DELAYHOURS * 60 * 60 * 1000;
+	}
 
 }
