c9e09f79f1be49e99d35f0d0e4f7ca87fe6b4f2e
ghuazh <ghuazh@gmail.com>
2013-05-06 23:24:29 +0800

    Signed-off-by: ghuazh <ghuazh@gmail.com>

---
+++ Demo_Upload/src/info/vidc/CertificateRequestService/CertificateInfo.java
@@ -0,0 +1,58 @@
+package info.vidc.CertificateRequestService;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+/**
+ * <p>
+ * Java class for CertificateInfo complex type.
+ * 
+ * <p>
+ * The following schema fragment specifies the expected content contained within
+ * this class.
+ * 
+ * <pre>
+ * &lt;complexType name=&quot;CertificateInfo&quot;&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;extension base=&quot;{http://www.vidc.info/certificate/operation/}CertificateWithSystemInfo&quot;&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name=&quot;VEHICLE_STATUS&quot; type=&quot;{http://www.w3.org/2001/XMLSchema}string&quot; minOccurs=&quot;0&quot;/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/extension&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "CertificateInfo", namespace = "http://www.vidc.info/certificate/operation/", propOrder = { "vehiclestatus" })
+public class CertificateInfo extends CertificateWithSystemInfo {
+
+	@XmlElement(name = "VEHICLE_STATUS")
+	protected String vehiclestatus;
+
+	/**
+	 * Gets the value of the vehiclestatus property.
+	 * 
+	 * @return possible object is {@link String }
+	 * 
+	 */
+	public String getVEHICLESTATUS() {
+		return vehiclestatus;
+	}
+
+	/**
+	 * Sets the value of the vehiclestatus property.
+	 * 
+	 * @param value
+	 *            allowed object is {@link String }
+	 * 
+	 */
+	public void setVEHICLESTATUS(String value) {
+		this.vehiclestatus = value;
+	}
+
+}
---
+++ Demo_Upload/src/info/vidc/CertificateRequestService/UploadInsertEntResponse.java
@@ -0,0 +1,60 @@
+package info.vidc.CertificateRequestService;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+/**
+ * <p>
+ * Java class for anonymous complex type.
+ * 
+ * <p>
+ * The following schema fragment specifies the expected content contained within
+ * this class.
+ * 
+ * <pre>
+ * &lt;complexType&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base=&quot;{http://www.w3.org/2001/XMLSchema}anyType&quot;&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name=&quot;UploadInsert_EntResult&quot; type=&quot;{http://www.vidc.info/certificate/operation/}OperateResult&quot; minOccurs=&quot;0&quot;/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = { "uploadInsertEntResult" })
+@XmlRootElement(name = "UploadInsert_EntResponse")
+public class UploadInsertEntResponse {
+
+	@XmlElement(name = "UploadInsert_EntResult")
+	protected OperateResult uploadInsertEntResult;
+
+	/**
+	 * Gets the value of the uploadInsertEntResult property.
+	 * 
+	 * @return possible object is {@link OperateResult }
+	 * 
+	 */
+	public OperateResult getUploadInsertEntResult() {
+		return uploadInsertEntResult;
+	}
+
+	/**
+	 * Sets the value of the uploadInsertEntResult property.
+	 * 
+	 * @param value
+	 *            allowed object is {@link OperateResult }
+	 * 
+	 */
+	public void setUploadInsertEntResult(OperateResult value) {
+		this.uploadInsertEntResult = value;
+	}
+
+}
